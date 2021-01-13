463f3deb1d85dad1ab500421c26f839f916470a1
Nicholas Levenson <nicklevenson@gmail.com>
2021-01-13 10:07:56 -0800

    object relations

--- app/models/mix.rb
+++ app/models/mix.rb
@@ -1,2 +1,8 @@
 class Mix < ApplicationRecord
+  belongs_to :user
+  has_many :mix_contents
+  has_many :contents, through: :mix_contents
+  has_many :likes
+  has_many :comments
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ApplicationRecord
+  has_secure_password
+  has_many :mixes
+  has_many :comments
+  has_many :likes
+  
+  validates :username, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :password, length: {minimum: 7}
 end
