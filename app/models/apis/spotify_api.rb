d92c36ff6edc2c02e75f6ffd898d6d07430d1e2c
Nicholas Levenson <nicklevenson@gmail.com>
2021-01-14 10:58:13 -0800

    spotify data refactor

--- app/models/apis/spotify_api.rb
+++ app/models/apis/spotify_api.rb
@@ -7,11 +7,11 @@ class SpotifyApi
       if result.type == 'album'
         filtered_data << {data_type: "music", title: result.name,creators: result.artists.collect{|c|c.name}, url: result.uri} unless result.album_type == "single" 
       elsif result.type == 'artist'
-        filtered_data << {data_type: "music", title: result.name,creators: result.name, url: result.uri}
+        filtered_data << {data_type: "music", title: result.name,creators: [result.name], url: result.uri}
       elsif result.type == 'track'
         filtered_data << {data_type: "music", title: result.name,creators: result.artists.collect{|c|c.name}, url: result.uri}
       elsif result.type == 'playlist'
-        filtered_data << {data_type: "music", title: result.name,creators: result.owner.display_name, url: result.uri}
+        filtered_data << {data_type: "music", title: result.name,creators: [result.owner.display_name], url: result.uri}
       end
       # byebug
     end
