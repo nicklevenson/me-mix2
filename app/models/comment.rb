1ad619c06fb8d69fa555a6a5a72e2c44435a8dd8
Nicholas Levenson <nicklevenson@gmail.com>
2021-01-15 10:42:30 -0800

    comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,7 +3,7 @@ class Comment < ApplicationRecord
   belongs_to :mix
   # has_rich_text :content
 
-  after_create_commit {broadcast_prepend_to "#{mix.id}comments", target: "#{mix.id}comments"}
+  after_create_commit {broadcast_append_to "#{mix.id}comments", target: "#{mix.id}comments"}
   after_update_commit {broadcast_replace_to "#{mix.id}comments"}
   after_destroy_commit {broadcast_remove_to "#{mix.id}comments"}
 end
