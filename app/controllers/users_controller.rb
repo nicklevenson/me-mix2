ab2f2bf09e14e323b9694247d2d3270575d7fc51
Nicholas Levenson <nicklevenson@gmail.com>
2021-01-18 14:44:28 -0800

    refactoring

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,7 @@ class UsersController < ApplicationController
   end
   def create
     @user = User.new(user_params)
+ 
     if @user.save
       session[:user_id] = @user.id
       redirect_to user_mixes_path(@user)
@@ -17,9 +18,24 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     redirect_to user_mixes_path(@user)
   end
+
+  def edit 
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.avatar = avatar_params[:avatar]
+    byebug
+    redirect_to user_mixes_path(@user)
+  end
   private
 
   def user_params
-    params.require(:user).permit(:username, :email, :password, :password_confirmation)
+    params.require(:user).permit(:username, :email, :password, :password_confirmation, :avatar)
+  end
+
+  def avatar_params
+    params.require(:user).permit(:avatar)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   has_many :mixes, :dependent => :destroy
   has_many :comments, :dependent => :destroy
   has_many :likes, :dependent => :destroy
+  has_one_attached :avatar
   
   validates :username, presence: true
   validates :email, presence: true, uniqueness: true
